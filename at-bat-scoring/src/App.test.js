13162c13d713571c2dfaf762badf378bf649f700
cladams0203 <cladams0203@gmail.com>
2019-10-11 22:49:00 -0400

    wrote tests for all three components and they passed

--- at-bat-scoring/src/App.test.js
+++ at-bat-scoring/src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
+import {Display} from './components/Display';
+import {Dashboard} from './components/Dashboard'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+afterEach(rtl.cleanup);
+test('renders without crashing', () => {
+  const wrapper = rtl.render(<App/>)
+  wrapper.debug()
 });
+test('passes', () => {
+  const wrapper = rtl.render(<Display />)
+  wrapper.debug()
+})
+test('passing', () => {
+  const wrapper = rtl.render(<Dashboard/>)
+  wrapper.debug()
+})
